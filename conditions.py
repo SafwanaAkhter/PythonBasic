2de8c51a4bf6e188e855b377f212071011f1365c
Safwana Akhter <safwana220151@gmail.com>
2026-03-14 20:01:49 +0600

    Initial Commit

---
+++ conditions.py
@@ -0,0 +1,11 @@
+name =('safwana')
+char=len(name)
+print(char)
+if char <3:
+    print('name must be atleast 3 characters ')
+
+elif char > 50:
+    print('name can be maximum of 50 characters')
+
+else:
+    print(f"name: {name} looks good!")
