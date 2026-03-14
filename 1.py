63f5961f8170029e051e964213a2100a3cd3dbda
Safwana Akhter <safwana220151@gmail.com>
2026-03-14 16:33:23 +0600

    Initial Commit

---
+++ 1.py
@@ -0,0 +1,12 @@
+is_hot=False
+is_cold=False
+
+if is_hot:
+    print('it is a hot day ')
+    print('drink plenty of water')
+elif is_cold:
+    print('it is a cold day ')
+    print('Wear warm cloths')
+
+else:
+    print('it is a lovely day')
---
+++ 2.py
@@ -0,0 +1,8 @@
+price=1000000
+good_credit= True
+if good_credit:
+    payment=price*0.1
+
+else:
+    payment= price* 0.2
+print(f"down payment: ${payment}")
